48c3d0be543e61f658dc1c80169702f26535d544
l33th <l33th@protonmail.ch>
2022-01-18 10:56:30 +0200

    api key setup

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const request = require('request-promise');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const apiKey = process.env.API_KEY;
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}`;
 
 app.use(express.json());
 
