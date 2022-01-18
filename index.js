7c8032232148e0eaf162a0e02422d81b8f6ff687
l33th <l33th@protonmail.ch>
2022-01-18 11:16:36 +0200

    offers route

--- index.js
+++ index.js
@@ -40,4 +40,17 @@ app.get('/products/:productId/reviews', async (req, res) => {
   }
 });
 
+// GET Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`); 
+      res.json(JSON.parse(response));
+  } catch (error) {
+      res.json(error)
+  }
+});
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
