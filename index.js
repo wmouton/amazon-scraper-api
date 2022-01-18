688d4b7ce075834ba0c7508b41de0797fdb9c303
l33th <l33th@protonmail.ch>
2022-01-18 11:05:48 +0200

    working api

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const apiKey = process.env.API_KEY;
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}`;
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the LTH-33 Amazon Scraper API');
+});
+
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`); 
+      res.json(response);
+  } catch (error) {
+      res.json(error)
+  }
 });
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
