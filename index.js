f7c0365ad7570a27085fe33d66bb8cc386db0b68
l33th <l33th@protonmail.ch>
2022-01-18 11:07:00 +0200

    organized info

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/products/:productId', async (req, res) => {
   try {
     const response = await request(
       `${baseUrl}&url=https://www.amazon.com/dp/${productId}`); 
-      res.json(response);
+      res.json(JSON.parse(response));
   } catch (error) {
       res.json(error)
   }
