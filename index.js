44fb6aa13fe26eceac136a55ce709ba93fe53479
l33th <l33th@protonmail.ch>
2022-01-18 11:10:18 +0200

    added reviews route

--- index.js
+++ index.js
@@ -27,4 +27,17 @@ app.get('/products/:productId', async (req, res) => {
   }
 });
 
+// GET Product Reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`); 
+      res.json(JSON.parse(response));
+  } catch (error) {
+      res.json(error)
+  }
+});
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
